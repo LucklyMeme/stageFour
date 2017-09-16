27810264c8517c4f3c98a3cd2a80d4ed75423eb0
WanLum <295386487@qq.com>
2017-09-16 19:48:21 +0800

    write QBH module one and module two include html and css

---
+++ H5C3/day01/Code/QBH/js/qbh.js
@@ -0,0 +1,10 @@
+$(function () {
+  // 初始化部分
+  $('#fullpage').fullpage({ // 调用函数
+
+    // 设置每一屏颜色
+    sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#fed", "#d04759", "#84d9ed", "#8ac060",'#84d9ed']
+
+  });
+
+});
