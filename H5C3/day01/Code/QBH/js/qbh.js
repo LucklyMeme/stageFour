d51045c6a48ef71a34c86d432dbfbea7302f7bff
WanLum <295386487@qq.com>
2017-09-19 10:04:58 +0800

    finish the QBH module one the name is qbh-list

--- H5C3/day01/Code/QBH/js/qbh.js
+++ H5C3/day01/Code/QBH/js/qbh.js
@@ -1,10 +1,9 @@
 $(function () {
   // 初始化部分
   $('#fullpage').fullpage({ // 调用函数
-
     // 设置每一屏颜色
     sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#fed", "#d04759", "#84d9ed", "#8ac060",'#84d9ed']
 
   });
-
+  
 });
